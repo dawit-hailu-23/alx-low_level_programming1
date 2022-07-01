c8295987a0ab5f440a9f4d164f80f62124fd97db
dawberry <dhailu128@gmail.com>
2022-07-01 22:59:35 +0300

    c for

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print first 10 num
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        int i = 0;
+
+        for (; i < 10; i++)
+        {
+                printf("%d", i);
+        }
+        printf("\n");
+        return (0);
+}
+
