0e6eafa17501edabdf690523c15c3fdfe1eb0a96
dawberry <dhailu128@gmail.com>
2022-07-01 23:24:46 +0300

    c for

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        int i = 0;
+	int i = 0;
 
-        for (; i < 10; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
+	for (; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
 }
 
