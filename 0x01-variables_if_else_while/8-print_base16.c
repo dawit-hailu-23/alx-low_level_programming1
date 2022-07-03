67ded3c8154faac3ccd80efcaffd1391308e4ed0
dawberry <dhailu128@gmail.com>
2022-07-03 13:23:56 +0300

    c for loop

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print in base 16
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i=0; i<=16; i++)
+	{
+		if(i > 9)
+		{
+			for (i='a';i<='f';i++)
+			{
+				putchar(i);
+			}
+		}
+		else
+			putchar(i+'0');
+	}
+	putchar('\n');
+}
