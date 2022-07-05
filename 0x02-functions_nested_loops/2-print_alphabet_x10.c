6234118b5584ec1308491415dfbdc7f48313bd3d
dawberry <dhailu128@gmail.com>
2022-07-05 23:14:13 +0300

    c function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabetX10 - print alphabet 10X
+ */
+void print_alphabet_x10(void)
+{
+	char j;
+	int i;
+
+	while (i <= 10)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
