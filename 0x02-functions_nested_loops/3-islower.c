a042042c59a695b2f25fd4a04fa05fb88163a33a
dawberry <dhailu128@gmail.com>
2022-07-06 21:37:02 +0300

    c function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 /**
  * _islower - check if char is lower or capital
+ * @c: character to check
  * Return: 1 if lower
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c <='z');
+	return (c >= 'a' && c <= 'z');
 }
