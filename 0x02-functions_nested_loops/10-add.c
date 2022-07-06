19ad4d7071490640e03426f42b27f6f9446e1ac6
dawberry <dhailu128@gmail.com>
2022-07-06 22:26:44 +0300

    c function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - add numbers
+ * @i: value
+ * @j: value
+ * Return: sum of number
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,8 +5,17 @@
  */
 int print_last_digit(int n)
 {
-	int last;
+	int a;
 
-	last = n % 10;
-	return last;
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
