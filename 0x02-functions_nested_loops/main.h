549f97a165bb02e1e085a043b45cda6391d2ac39
dawberry <dhailu128@gmail.com>
2022-07-06 22:11:46 +0300

    c function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 int _putchar(char);
 int print_sign(int);
+int print_sign(int);
+int print_last_digit(int);
+int _abs(int);
 int _isalpha(int);
 int _islower(int);
 void print_alphabet_x10(void);
