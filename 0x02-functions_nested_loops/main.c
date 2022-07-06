78241c94d14028306b294b6929d082f9862566ba
dawberry <dhailu128@gmail.com>
2022-07-06 21:52:21 +0300

    c function

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -2,8 +2,11 @@
 int main()
 {
 	int r;
+	int c;
 
-	r = _islower('H');
+	r = _isalpha('a');
+	c = _isalpha(4);
+	_putchar(c + '0');
 	_putchar(r+'0');
 	return 0;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char);
+int _isalpha(int);
 int _islower(int);
 void print_alphabet_x10(void);
 void print_alphabet(void);
