427f805275e07a0e836a961338d54c5c59de7aaf
dawberry <dhailu128@gmail.com>
2022-07-07 23:35:50 +0300

    c more function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,23 @@ int main(void)
 
 	while (i <= 100)
 	{
-		//if(i<100)
-		//{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz ");
-		}else if (i % 5 == 0)
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz ");
-		}else if (i % 5 == 0 && i % 3 == 0)
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		}else
+			printf(" FizzBuzz");
+		} else if (i == 1)
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
 		}
-		//}
 		i++;
 	}
+	printf("\n");
 }
