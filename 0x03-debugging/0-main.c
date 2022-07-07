7d789dc36eb82d5c26fe8ad3930aa68fea9db8ad
dawberry <dhailu128@gmail.com>
2022-07-08 00:20:03 +0300

    debuging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - print positive if number is + and negative if num is -
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	positive_or_negative();
+	return (0);
+}
