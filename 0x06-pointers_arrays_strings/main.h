fee45f1650fea868788778fd7d45705e568cf7aa
dawberry <dhailu128@gmail.com>
2022-07-15 20:52:11 +0300

    c more pointer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,4 +6,6 @@ int _strcmp(char, char);
 char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
+char *_strncpy(char *dest, char *src, int n);
+void print_number(int);
 #endif
