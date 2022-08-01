99b93a4ff38372a6113fb0556523d110618bd303
dawberry <dhailu128@gmail.com>
2022-08-01 22:45:44 +0300

    c struct and typedef learning

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - set dog struct values
+ * @d: structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+void init_dog(struct dog *d, char* name , float age, char* owner)
+{
+    (*d).name = name;
+    d->age = age;
+    d->owner = owner;
+}
