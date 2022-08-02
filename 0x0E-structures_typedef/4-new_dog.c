98a489039251bd024997273cadd8bd71b0758a5d
dawberry <dhailu128@gmail.com>
2022-08-02 11:52:34 +0300

    c typedef and struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ * new_dog - set value of dog_t
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,11 @@
  * @age: dog age
  * @owner: dog owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
