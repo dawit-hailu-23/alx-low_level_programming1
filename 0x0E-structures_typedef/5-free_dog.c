fe934d77e7a7de1b6ecab2dc3737b0ffa27f7650
dawberry <dhailu128@gmail.com>
2022-08-02 12:32:08 +0300

    c typedef and struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dog struct
+ * @d: struct of dog
+ */
+void free_dog(dog_t d)
+{
+	if (d != NULL)
+		return (NULL);
+	free(d->name);
+	free(d->owner);
+	free(d->age);
+	free(d);
+}
