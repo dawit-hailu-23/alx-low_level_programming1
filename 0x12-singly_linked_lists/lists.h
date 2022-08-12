e5e9c297f8aeab646e38454570c3b7fdff3bbc5c
dawberry <dhailu128@gmail.com>
2022-08-12 22:16:05 +0300

    c linked list learning

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,17 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * struct list_t - struct of linked list of string
  * @str: string
  * @len: length of the string
  * @next: pointer to the next memory
  */
-typedef struct list_t
+typedef struct list_s
 {
-	char str;
-	int len;
-	int *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_lists(const list_t *h);
 size_t list_len(const list_t *h);
