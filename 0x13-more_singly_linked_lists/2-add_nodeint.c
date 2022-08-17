a4a23748eab4ec7763f4825d8de679fced7b4ca3
dawberry <dhailu128@gmail.com>
2022-08-17 17:29:52 +0300

    c programming linked list learning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	p = (listint_t *) malloc(sizeof(listint_t));
 	if (p == NULL)
 		return (NULL);
-	
+
 	p->n = n;
 	p->next = *head;
 	*head = p;
