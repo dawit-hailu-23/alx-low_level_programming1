6e5df7a67ee1cd80e59cd40970a385f759d66127
dawberry <dhailu128@gmail.com>
2022-09-08 22:21:17 +0300

    c file i/o learning

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
 #define PERM 0600
 
 /**
- * create_file - creates file 
+ * create_file - creates file
  * @filename: the file name
  * @text_content: the text to write to the file
  *
@@ -14,11 +14,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	char buff[sizeof(text_content)];
-	
+
 	strcat(buff, text_content);
 	fd = creat(filename, 0600);
 
-	if (write(fd, buff,  sizeof(buff)) == -1)
+	if (write(fd, buff, sizeof(buff)) == -1)
 		return (0);
 	return (1);
 }
